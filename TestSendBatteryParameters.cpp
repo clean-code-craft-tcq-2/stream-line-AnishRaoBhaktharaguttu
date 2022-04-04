83a2bc6ec9d7fd4d112d1c898054aa0866a33124
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-04-04 17:51:41 +0530

    Update TestSendBatteryParameters.cpp

--- TestSendBatteryParameters.cpp
+++ TestSendBatteryParameters.cpp
@@ -1,3 +1,13 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test-framework/catch.hpp"
+#include "SendBatteryParameters.h"
+
+
+TEST_CASE("Test to check Sender") 
+{
+  BatteryChargingParameters batteryChargingParameters;
+  readBatteryParametersFromFile(batteryChargingParameters);
+  // Send the battery parameters to console
+  batteryParametersSender(batteryChargingParameters);
+}
