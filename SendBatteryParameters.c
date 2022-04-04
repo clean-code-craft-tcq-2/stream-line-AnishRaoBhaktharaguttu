82d4d77e1ae8f8138d515a1d8f83c49addf5da0d
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-04-04 16:02:44 +0530

    Update SendBatteryParameters.c

--- SendBatteryParameters.c
+++ SendBatteryParameters.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "SendBatteryParameters.h"
+
+void readBatteryParametersFromFile(BatteryChargingParameters *batteryParameters)
+{
+  float temperature, stateOfCharge;
+    FILE * parametersFile = fopen("batteryParameters.txt","r");  
+    if (parametersFile!=NULL) {
+        for(int j=0;fscanf(parametersFile, "%f\t%f\n", &temperature,&StateOfCharge)!=EOF ;j++)
+        {
+            batteryParameters->temperature = temperature;
+            batteryParameters->stateOfCharge = stateOfCharge;
+            batteryParameters++;
+        }
+    }
+    fclose(file);  
+}
