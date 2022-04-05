3d1e52c10e293917bc82c74c02313051137cb18d
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-04-05 13:28:51 +0530

    Update TestSendBatteryParameters.cpp

--- TestSendBatteryParameters.cpp
+++ TestSendBatteryParameters.cpp
@@ -12,10 +12,10 @@ TEST_CASE("Test to check Sender")
   float expectedBatteryParameters[2][2] = {{3.422, 11.065}, {1.345, 64.528}};
 
    REQUIRE(batteryChargingParameters[0].temperature = expectedBatteryParameters[0][0]);
-   REQUIRE(StateOfCharge[batteryChargingParameters[0].stateOfCharge == expectedBatteryParameters[0][1]);
+   REQUIRE(batteryChargingParameters[0].stateOfCharge == expectedBatteryParameters[0][1]);
 
    REQUIRE(batteryChargingParameters[49].temperature = expectedBatteryParameters[1][0]);
-   REQUIRE(StateOfCharge[batteryChargingParameters[49].stateOfCharge == expectedBatteryParameters[1][1]);
+   REQUIRE(batteryChargingParameters[49].stateOfCharge == expectedBatteryParameters[1][1]);
                          
   // Send the battery parameters to console
   batteryParametersSender(batteryChargingParameters);
