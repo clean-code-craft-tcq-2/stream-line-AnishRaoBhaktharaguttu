89c7e1a2eb1f9cd53d608fc1348e5440475574a2
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-11 15:45:10 +0530

    Update TestReceiveBatteryParameters.cpp

--- TestReceiveBatteryParameters.cpp
+++ TestReceiveBatteryParameters.cpp
@@ -1,1 +1,14 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
+#include "test-framework/catch.hpp"
+#include "SendBatteryParameters.h"
+
+
+TEST_CASE("Test to check Receiver") 
+{
+   BatteryChargingParameters ReceivedData;
+   printf("Temperature: 3.420 deg C , State of Charge: 11.060\n");
+   batteryParametersReceiver(ReceivedData);
+   REQUIRE(ReceivedData.temperature==3.420);
+   REQUIRE(ReceivedData.stateOfCharge==11.060);
+}
