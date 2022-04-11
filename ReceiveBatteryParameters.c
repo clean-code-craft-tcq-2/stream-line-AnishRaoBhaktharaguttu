7513ffb5e7ed1902e89579d50175b87414b56b1b
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-11 16:58:47 +0530

    Create ReceiveBatteryParameters.c

---
+++ ReceiveBatteryParameters.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "Receiver.h"
+#include "SendBatteryParameters.h"
+
+void batteryParametersReceiver(BatteryChargingParameters *batteryParameters)
+{
+
+}
