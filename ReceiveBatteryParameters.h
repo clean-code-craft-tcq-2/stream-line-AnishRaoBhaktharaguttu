9e8e0fab61a8d7242b8a55f62c863216becc7342
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-12 16:46:40 +0530

    Update ReceiveBatteryParameters.h

--- ReceiveBatteryParameters.h
+++ ReceiveBatteryParameters.h
@@ -1,2 +1,7 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include "SendBatteryParameters.h"
 void batteryParametersReceiver(BatteryChargingParameters *batteryParameters);
