9666f7a10e7c2fb0c645f4d1a45a4cc3740870f1
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-12 16:28:15 +0530

    Update ReceiveBatteryParameters.c

--- ReceiveBatteryParameters.c
+++ ReceiveBatteryParameters.c
@@ -2,37 +2,14 @@
 #include <math.h>
 #include <stdlib.h>
 #include "ReceiveBatteryParameters.h"
-
+float Temp[BMS_DATA] = {};
+float Soc[BMS_DATA] = {};
 void ReadDataFromConsole(BatteryChargingParameters *batteryParameters)
 {	
-    char buffer[1000];
-    size_t line = 0;
-    for (int i = 0; i < 50; i++)
+        for (int i = 0; i < 50; i++)
 	{
-         scanf("%s\n", &buffer);
-         printf("%s\n", buffer);
+		scanf("%f\t%f\n", &Temp[i], &Soc[i]);
 	}
-    while ((fgets(buffer, sizeof buffer, stdin) != NULL)) {
-        printf("%s\n",buffer);
-        if (sscanf(buffer, "%.3f   %.3f", batteryParameters->temperature,batteryParameters->stateOfCharge) != 1) {
-            fprintf(stderr, "Line formatting error\n");
-            exit(EXIT_FAILURE);
-        }
-        batteryParameters++;
-        ++line;
-    }
-
-    for (size_t i = 0; i < line; i++) {
-        printf("%.3f \t %.3f\n", batteryParameters->temperature,batteryParameters->stateOfCharge);
-        batteryParameters++;
-    }
-    /*char buffer[1000];
-    for (int i = 0; i < 50; i++)
-	{
-         scanf("%.3f\t%.3f\n", batteryParameters->temperature,batteryParameters->stateOfCharge);
-         printf("read: %.3f\t read:%.3f \n", batteryParameters->temperature,batteryParameters->stateOfCharge);
-         batteryParameters++;
-	}*/
 }
 
 void batteryParametersReceiver(BatteryChargingParameters *batteryParameters)
