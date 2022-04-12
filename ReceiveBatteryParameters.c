53ce7de7d0e8ef516637e64b2e60820209956a21
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-12 09:32:03 +0530

    Update ReceiveBatteryParameters.c

--- ReceiveBatteryParameters.c
+++ ReceiveBatteryParameters.c
@@ -5,14 +5,32 @@
 
 void ReadDataFromConsole(BatteryChargingParameters *batteryParameters)
 {	
-    char buffer[100];
-    while(fgets(buffer, sizeof buffer, stdin) != NULL) {
-        if(sscanf(buffer, "%.3f", batteryParameters->temperature) != 1) {    // or strtol perhaps
-            break;
-        }
-        printf("Number: %d\n", batteryParameters->temperature);
-	batteryParameters++;
-    }
+    char *buf = NULL;
+    int size;
+    unsigned int length;
+    size = getline(&buf, &length, stdin);
+    if (size != -1)
+        /* do anything you want with the stuff that was entered here */
+        /* for the example I just write it back to stdout! */
+        puts(buf);
+    else
+        /* this would be your "end" condition */
+        printf("Nothing read!\n");
+
+    printf("Size: %d\n Length: %d\n", size, length);
+    free(buf);
+	
+	
+    //char buffer[100];
+    //while(fgets(buffer, sizeof buffer, stdin) != NULL) {
+        //if(sscanf(buffer, "%.3f", batteryParameters->temperature) != 1) {    // or strtol perhaps
+           // break;
+       // }
+      //  printf("Number: %d\n", batteryParameters->temperature);
+	//batteryParameters++;
+  //  }
+	
+	
   //for (int i = 0; i < STREAM_SIZE; i++)
 	//{
 	//	scanf("%f\t%f\n", batteryParameters->temperature, batteryParameters->stateOfCharge);
