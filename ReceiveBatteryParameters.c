393f98904e9f1c051be73dd98ebd3bf7b9b097aa
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-12 15:28:30 +0530

    Update ReceiveBatteryParameters.c

--- ReceiveBatteryParameters.c
+++ ReceiveBatteryParameters.c
@@ -5,7 +5,21 @@
 
 void ReadDataFromConsole(BatteryChargingParameters *batteryParameters)
 {	
-  
+    char buffer[1000];
+    int arr[STREAM_SIZE];
+    size_t line = 0;
+
+    while ((fgets(buffer, sizeof buffer, stdin) != NULL)) {
+        if (sscanf(buffer, "%d", &arr[line]) != 1) {
+            fprintf(stderr, "Line formatting error\n");
+            exit(EXIT_FAILURE);
+        }
+        ++line;
+    }
+
+    for (size_t i = 0; i < line; i++) {
+        printf("%5d\n", arr[i]);
+    }
 }
 
 void batteryParametersReceiver(BatteryChargingParameters *batteryParameters)
