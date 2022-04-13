dcc872b02646d09fd6a6de49501335731d09ef9e
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-13 16:34:25 +0530

    Update ReceiveBatteryParameters.c

--- ReceiveBatteryParameters.c
+++ ReceiveBatteryParameters.c
@@ -13,14 +13,14 @@ void ReadDataFromConsole(BatteryChargingParameters *batteryParameters)
         {
             break;
         }
-        scanf("%f", batteryParameters->temperature);
+        scanf("%f", &(batteryParameters->temperature));
         scanf("%50s", ReadString); //deg
         scanf("%50s", ReadString); //C
         scanf("%50s", ReadString); //,
         scanf("%50s", ReadString); // State
         scanf("%50s", ReadString); //of
         scanf("%50s", ReadString); //charge:
-        scanf("%f", batteryParameters->stateOfCharge);
+        scanf("%f", &(batteryParameters->stateOfCharge));
 	printf("%.3f \t %.3f\n",batteryParameters->temperature,batteryParameters->stateOfCharge);
 	batteryParameters++;
    }
