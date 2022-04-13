d54ab31ccd025fc8328a387b6eebd90e6c5d874d
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-13 13:19:13 +0530

    Update ReceiveBatteryParameters.c

--- ReceiveBatteryParameters.c
+++ ReceiveBatteryParameters.c
@@ -16,7 +16,7 @@ void ReadDataFromConsole(BatteryChargingParameters *batteryParameters)
 		break;
 	}
 	printf("HI 4\n");
-	//scanf("%50s", ReadString);
+	scanf("%50s", ReadString);
         scanf("%f", batteryParameters->temperature);
         scanf("%50s", ReadString); //deg
         scanf("%50s", ReadString); //C
@@ -25,7 +25,7 @@ void ReadDataFromConsole(BatteryChargingParameters *batteryParameters)
         scanf("%50s", ReadString); //of
         scanf("%50s", ReadString); //charge:
         scanf("%f", batteryParameters->stateOfCharge);
-	printf("HI: %.2f\t%.2f\n",batteryParameters->temperature,batteryParameters->stateOfCharge);
+	printf("HI: 5\n",batteryParameters->temperature,batteryParameters->stateOfCharge);
 	batteryParameters++;
     }
 }
