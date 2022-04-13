2af823cb85ec22d53609b75d48c690b86dccb546
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-13 13:44:47 +0530

    Update TestReceiveBatteryParameters.cpp

--- TestReceiveBatteryParameters.cpp
+++ TestReceiveBatteryParameters.cpp
@@ -6,7 +6,7 @@
 TEST_CASE("Test to check Receiver") 
 {
    BatteryChargingParameters ReceivedData[STREAM_SIZE];
-   for(int i - 0 ; i < STREAM_SIZE ;i++)
+   for(int i = 0 ; i < STREAM_SIZE ;i++)
    {
    printf("Temperature: 3.420 deg C , State of Charge: 11.060\n");
    }
