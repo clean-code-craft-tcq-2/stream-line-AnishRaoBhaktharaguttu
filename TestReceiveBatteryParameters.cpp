5f433fa932c26c748d13a94cab02d9911c9896d7
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-28 13:59:21 +0530

    Update TestReceiveBatteryParameters.cpp

--- TestReceiveBatteryParameters.cpp
+++ TestReceiveBatteryParameters.cpp
@@ -7,7 +7,6 @@
 TEST_CASE("Test to check Receiver") 
 { 
    BatteryChargingParameters ReceivedData[STREAM_SIZE], ExpectedData[STREAM_SIZE], Max, Min,Avg,MovingAvg[STREAM_SIZE];
-   BatteryChargingParameters *MvgAvgPtr = MovingAvg;
    ReadDataFromConsole(ReceivedData);
    float Temperature_loc, StateOfCharge_loc;
     
@@ -30,7 +29,8 @@ TEST_CASE("Test to check Receiver")
     Min= ComputeMinBMS_Data(ReceivedData);
     Max = ComputeMaxBMS_Data(ReceivedData);
     Avg = ComputeAvgBMS_Data(ReceivedData);
-    MvgAvgPtr = ComputeMovingAvgBMS_Data(ReceivedData,5);
+    
+    ComputeMovingAvgBMS_Data(ReceivedData,MovingAvg,5);
    
     REQUIRE( abs(Min.temperature - 1.030 ) <=0.001);
     REQUIRE( abs(Max.temperature - 47.960 ) <=0.001);
