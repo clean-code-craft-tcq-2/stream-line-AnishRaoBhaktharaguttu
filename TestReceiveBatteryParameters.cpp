ea966e03693fad4b700e94e5dbac6973993822d1
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-28 13:13:26 +0530

    Update TestReceiveBatteryParameters.cpp

--- TestReceiveBatteryParameters.cpp
+++ TestReceiveBatteryParameters.cpp
@@ -29,7 +29,7 @@ TEST_CASE("Test to check Receiver")
     Min= ComputeMinBMS_Data(ReceivedData);
     Max = ComputeMaxBMS_Data(ReceivedData);
     Avg = ComputeAvgBMS_Data(ReceivedData);
-    ComputeMovingAvgBMS_Data(ReceivedData);
+    ComputeMovingAvgBMS_Data(ReceivedData,5);
    
     REQUIRE( abs(Min.temperature - 1.030 ) <=0.001);
     REQUIRE( abs(Max.temperature - 47.960 ) <=0.001);
